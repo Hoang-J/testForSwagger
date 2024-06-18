a154d8fb019fa454e2787adc589b9e5dab22fdb2
COR Coding <corcoding@LAPTOP-N1N1CVP7.localdomain>
2024-06-18 13:50:17 -0400

    added css

--- npm/index.js
+++ npm/index.js
@@ -48,7 +48,10 @@ const options = {
   app.use(
     "/api-docs",
     swaggerUi.serve,
-    swaggerUi.setup(specs, { customCssUrl: CSS_URL })
+    swaggerUi.setup(specs, { 
+      customCssUrl: CSS_URL,
+      customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }'
+    })
 );
 
 
